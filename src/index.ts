051bc7ccd6ba6899f7ead11eb7042ee6fbebc933
Sebastian Asis <sasisromero10@gmail.com>
2022-03-21 15:30:24 -0500

    add cors module

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/pets", petsRoutes);
